a16b7c8fa672d2d9cab7b0af91eae5ccb04529b3
milkylake <ajnaliev07@gmail.com>
2026-03-24 10:47:45 +0300

    feat: add grpc integration config and env variables

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,13 +1,13 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule } from '@nestjs/config';
 import { ConfigService } from './config.service';
-import { appConfig, databaseConfig } from './configs';
+import { appConfig, databaseConfig, grpcConfig } from './configs';
 
 @Global()
 @Module({
   imports: [
     NestConfigModule.forRoot({
-      load: [appConfig, databaseConfig],
+      load: [appConfig, databaseConfig, grpcConfig],
       envFilePath: '.env',
     }),
   ],
